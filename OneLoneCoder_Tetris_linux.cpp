401de4c90fc5793d4945eb8105b1aff3e194597e
Yuting Wang <yuting.wang@yale.edu>
2018-11-22 21:20:57 -0500

    Set reaction time

--- OneLoneCoder_Tetris_linux.cpp
+++ OneLoneCoder_Tetris_linux.cpp
@@ -76,7 +76,7 @@ void init_ncurses ()
   keypad(stdscr, TRUE);		/* I need that nifty F1 	*/
   noecho();
   curs_set(0);
-  nodelay(stdscr, TRUE);
+  timeout(10);
   start_color();
   init_pair(1,COLOR_WHITE, COLOR_BLACK);
   init_pair(2,COLOR_RED, COLOR_BLACK);
